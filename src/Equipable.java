591a663d93ee127661991a2273293fa0c19a566f
Elijah <117547888+c00l-us3r@users.noreply.github.com>
2023-05-17 13:46:58 -0700

    Weapon class & updated Equipable class

--- src/Equipable.java
+++ src/Equipable.java
@@ -4,8 +4,8 @@
  * like Defense and Strength. This consists of Armor and Weapons.
  *
  * @author Elijah Johnson
- * @version 1.0
- * @since 5.6.2023
+ * @version 1.1
+ * @since 5.17.2023
  */
 public class Equipable extends Item {
 
@@ -15,7 +15,7 @@ public class Equipable extends Item {
     /**determines which armor slot the Equipable will go*/
     private Slot slot;
 
-    /**the modifiers for the player's stats*/
+    /**the modifiers for the player's stats, in order: Max HP, STR, DEF, LUCK*/
     private int[] mods;
 
     /**how many times the Equipable can be used/hit*/
@@ -27,7 +27,7 @@ public class Equipable extends Item {
      * @param desc description for the Item
      * @param size "size" or "weight" for the Item
      * @param slot which slot the Item will equip to
-     * @param mods the modifiers for the Player's stats
+     * @param mods the modifiers for the Player's stats. In order: Max HP, STR, DEF, LUCK
      * @param durability the number of uses or hits the Item has
      */
     public Equipable(String name, String desc, int size, Slot slot, int[] mods, int durability){
@@ -62,6 +62,17 @@ public class Equipable extends Item {
     }
 
     /***
+     * Setter to set a new durability - used when a hit is successful
+     * @param x the new value of durability
+     */
+    public void setDurability(int x){
+        durability = x;
+        if(durability<=0){
+            unequip();
+        }
+    }
+
+    /***
      * Method to call when the Player tries to equip an item.
      * @return true if the item will equip, false if the item cannot be equipped
      */
@@ -69,6 +80,12 @@ public class Equipable extends Item {
         //code will come later when things are more fleshed out
         return true;
     }
-
-
+    /***
+     * Method to call when the Player tries to unequip an item.
+     * @return true if the item will equip, false if the item cannot be equipped
+     */
+    public boolean unequip(){
+        //code will come later when things are more fleshed out
+        return true;
+    }
 }
---
+++ src/Weapon.java
@@ -0,0 +1,43 @@
+/***
+ * Subclass of Equipable. While this class doesn't add much,
+ * it has a specific hit() method that will be different from
+ * Armor hit() method.
+ *
+ * @author Elijah Johnson
+ * @version 1.0
+ * @since 5.17.2023
+ */
+public class Weapon extends Equipable {
+    /***
+     * The Constructor for a Weapon object.
+     * @param name name for the Item
+     * @param desc description for the Item
+     * @param size "size" or "weight" for the Item
+     * @param slot which slot the Item will equip to
+     * @param mods the modifiers for the Player's stats. In order: Max HP, STR, DEF, LUCK
+     * @param durability the number of uses or hits the Item has
+     */
+    public Weapon(String name, String desc, int size, Slot slot, int[] mods, int durability){
+        super(name, desc, size, slot, mods, durability);
+    }
+
+    /***
+     * Method to be called when the Player attempts to hit an enemy.
+     * @param playerAcc the Player's ACC stat
+     * @param playerStr the Player's STR stat
+     * @param enemyAvo  the Enemy's AVO stat
+     * @return the damage dealt to the enemy
+     */
+    public int hit(int playerAcc, int playerStr, int enemyAvo){
+
+        int hitChance = playerAcc + this.getMods()[3]; //Player ACC stat + Weapon LUCK stat
+        int damage = 0;
+
+        if(hitChance > enemyAvo){                       //hits if hitChance is greater than enemyAvo
+            damage = playerStr + this.getMods()[1];     //Player STR stat + Weapon STR stat
+            this.setDurability(this.getDurability()-1);
+        }
+
+        return damage;
+    }
+}
