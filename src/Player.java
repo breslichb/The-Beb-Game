3b66468f723274dc6df1f7c5ee0aefc575c50d3d
Bayasgalan <battogtokhb@cwu.edu>
2023-05-17 14:11:49 -0700

    updated a Player class added a talk

--- src/Player.java
+++ src/Player.java
@@ -1,35 +1,37 @@
+import java.util.Scanner;
+
 public class Player extends Mobs {
-    private int headArmor;
-    private int bodyArmor;
-    private int armsArmor;
-    private int legsArmor;
+    private Armor head;
+    private Armor body;
+    private Armor arms;
+    private Armor legs;
     private Weapon[] hands;
     private List<Item> pack = new ArrayList<>();
     private List<Quest> quests = new ArrayList<>();
 
-    public Player(int hp, int def, int attack, String name, String description, int headArmor, int bodyArmor, int armsArmor, int legsArmor, Weapon[] hands) {
+    public Player(int hp, int def, int attack, String name, String description, Armor head, Armor body, Armor arms, Armor legs, Weapon[] hands) {
         super(hp, def, attack, name, description);
-        this.headArmor = headArmor;
-        this.bodyArmor = bodyArmor;
-        this.armsArmor = armsArmor;
-        this.legsArmor = legsArmor;
+        this.head = head;
+        this.body = body;
+        this.arms = arms;
+        this.legs = legs;
         this.hands = hands;
     }
 
-    public int getHeadArmor() {
-        return headArmor;
+    public Armor getHead() {
+        return head;
     }
 
-    public int getBodyArmor() {
-        return bodyArmor;
+    public Armor getBody() {
+        return body;
     }
 
-    public int getArmsArmor() {
-        return armsArmor;
+    public Armor getArms() {
+        return arms;
     }
 
-    public int getLegsArmor() {
-        return legsArmor;
+    public Armor getLegs() {
+        return legs;
     }
 
     public Weapon[] getHands() {
@@ -44,23 +46,27 @@ public class Player extends Mobs {
         return quests;
     }
 
-    public boolean talk() {
-
-    }
-
     public boolean walk(Direction direction) {
-
     }
 
     public void inventory() {
-
     }
 
     public void quests() {
-
     }
 
-    public boolean interact() {
+    public boolean talk() {
+        System.out.println("Player: Hello!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("NPC: How can I help you today? ");
+        String response = scanner.nextLine();
+        if (response.equalsIgnoreCase("yes")) {
+            System.out.println("Player: Sure, I'm interested in a quest.");
 
+            return true;
+        } else {
+            System.out.println("Player: Not right now, maybe later.");
+            return false;
+        }
     }
 }
