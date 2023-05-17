7ce2e616741146b037355467d8b5ef038b876465
Bayasgalan <battogtokhb@cwu.edu>
2023-05-17 14:12:42 -0700

    updated a Quest class

--- src/Quest.java
+++ src/Quest.java
@@ -1,14 +1,45 @@
 public class Quest {
-    private questDes description;
+    private QuestType type;
+    private String description;
+    private int requiredEnemyCount;
+    private int currentEnemyCount;
 
-    public Quest(questDes description) {
+    public Quest(QuestType type, String description) {
+        this.type = type;
         this.description = description;
+        this.requiredEnemyCount = 0;
+        this.currentEnemyCount = 0;
     }
 
-    public questDes getDescription() {
+    public QuestType getType() {
+        return type;
+    }
+
+    public String getDescription() {
         return description;
     }
-    public enum questDes {
+
+    public int getRequiredEnemyCount() {
+        return requiredEnemyCount;
+    }
+
+    public void setRequiredEnemyCount(int requiredEnemyCount) {
+        this.requiredEnemyCount = requiredEnemyCount;
+    }
+
+    public int getCurrentEnemyCount() {
+        return currentEnemyCount;
+    }
+
+    public void incrementEnemyCount() {
+        currentEnemyCount++;
+    }
+
+    public boolean isQuestCompleted() {
+        return currentEnemyCount >= requiredEnemyCount;
+    }
+
+    public enum QuestType {
         KILL_ENEMIES,
         COLLECT_ITEMS,
     }
