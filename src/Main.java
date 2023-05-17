5e93ae9d98a04504a53b2783b867d43bcc6511b0
Elijah <117547888+c00l-us3r@users.noreply.github.com>
2023-05-17 16:42:46 -0700

    finished up the methods - NOT TESTED

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static Potion createPotion(){
         Random rand = new Random();
-        String[] type = new String[]{"HP", "STR", "DEF", "LUCK");
+        String[] type = new String[]{"HP", "STR", "DEF", "LUCK"};
         int typeNum = rand.nextInt(4);
         int useSize = rand.nextInt(3)+1;
         int effect = rand.nextInt(9)+2;
         int duration;
-        if(typeNum = 0){
+        if(typeNum == 0){
             duration=0;
         }else{
             duration = rand.nextInt(5)+1;
@@ -31,50 +31,61 @@ public class Main {
         return new Weapon(types[typeNum], "A weapon.", size, mods, durability);
     }
 
-    public static Armor createWeapon(){
+    public static Armor createArmor(){
         Random rand = new Random();
         int HP=0;
-        if(rand.nextBool()){HP = rand.nextInt(10)+1;}
+        if(rand.nextBoolean()){HP = rand.nextInt(10)+1;}
 
-        Equipable.Slot slot;
+        Equipable.Slot slot = null;
+        int STR=0;
+        int DEF=0;
+        int size=0;
+        int durability=0;
+        String name="";
         int slotNum = rand.nextInt(4)+1;
         switch(slotNum){
             case 1:
                 slot = Equipable.Slot.HEAD;
+                STR = 0;
+                DEF = rand.nextInt(5)+1;
+                size = rand.nextInt(6)+5;
+                durability = rand.nextInt(6)+5;
+                name = "Helmet";
                 break;
             case 2:
                 slot = Equipable.Slot.CHEST;
+                STR = rand.nextInt(3);
+                DEF = rand.nextInt(10)+1;
+                size = rand.nextInt(11)+10;
+                durability = rand.nextInt(11)+10;
+                name = "Chestplate";
                 break;
             case 3:
                 slot = Equipable.Slot.LEGS;
+                STR = 0;
+                DEF = rand.nextInt(3)+1;
+                size = rand.nextInt(4)+3;
+                durability = rand.nextInt(4)+3;
+                name = "Boots";
                 break;
             case 4:
                 slot = Equipable.Slot.ARMS;
+                STR = rand.nextInt(5)+1;
+                DEF = rand.nextInt(5)+1;
+                size = rand.nextInt(4)+3;
+                durability = rand.nextInt(4)+3;
+                name = "Gauntlets";
                 break;
         }
 
-        int[] mods = new int[]{
-        int size =
-        int durability =
+        int[] mods = new int[]{HP, STR, DEF, rand.nextInt(10)+1};
 
-        return new Weapon(types[typeNum], "An armor piece.", size, mods, durability);
+        return new Armor(name, "An armor piece.", size, slot, mods, durability);
     }
-
-    public static Armor createArmor(){
-
-
-    }
+    */
 
 
-
-
-
-
-
-
-     */
-
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         // Main UI stuff should go here.
     }
 }
