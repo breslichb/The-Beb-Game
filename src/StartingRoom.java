b5ead0b3d0de9fcc51a601fc65dfb39ae24c964f
Ben <bbreslicH@gmail.com>
2023-05-27 18:32:37 -0700

    Edits a desc

--- src/StartingRoom.java
+++ src/StartingRoom.java
@@ -1,6 +1,6 @@
 public class StartingRoom extends Room{
     public StartingRoom(GameMap parent) {
         super(parent);
-        desc = "There's a hatch in the ceiling, like the one you climbed in through.";
+        desc = "There's a hatch in the ceiling. It is closed.";
     }
 }
