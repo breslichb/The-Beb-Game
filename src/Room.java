4d9596317c9128d82ce835120151a4ba2ca6ae83
Ben <bbreslicH@gmail.com>
2023-05-29 16:10:54 -0700

    Adds a thing

--- src/Room.java
+++ src/Room.java
@@ -86,6 +86,11 @@ public abstract class Room implements Serializable {
      * @return a string describing the interaction.
      */
     public String interact(){
+        if(items.size() > 0) {
+            Item i = items.get(0);
+            parentMap.getPlayer().addToInventory(i);
+            removeItem(i);
+        }
         return "You don't see anything particularly interesting you can do with this room.";
     }
 
